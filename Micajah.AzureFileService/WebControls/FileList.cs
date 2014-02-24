88c403680835a5f3b425681e810f2cfba4bc5585
yuranet <yura.yuriev@gmail.com>
2014-02-24 18:25:28 +0400

    Fix the issue with FileManager in the controls.

--- Micajah.AzureFileService/WebControls/FileList.cs
+++ Micajah.AzureFileService/WebControls/FileList.cs
@@ -470,7 +470,7 @@ namespace Micajah.AzureFileService.WebControls
 
                 if (m_FileManager != null)
                 {
-                    m_FileManager.ContainerPublicAccess = value;
+                    m_FileManager.ContainerName = this.ContainerName;
                 }
             }
         }
--- Micajah.AzureFileService/WebControls/FileUpload.cs
+++ Micajah.AzureFileService/WebControls/FileUpload.cs
@@ -186,7 +186,7 @@ namespace Micajah.AzureFileService.WebControls
 
                 if (m_FileManager != null)
                 {
-                    m_FileManager.ContainerPublicAccess = value;
+                    m_FileManager.ContainerName = this.ContainerName;
                 }
             }
         }
