4e7cacf4d059bf5693b6d481ca476e905e0e6623
vgooz <vladimir.gooz@gmail.com>
2018-08-17 18:05:16 +0300

    Configured use powershell script to create and publish nuget package to the VSTS Micajah packages repository

---
+++ Micajah.AzureFileService/CreateAndPublishNugetPackage.ps1
@@ -0,0 +1,8 @@
+﻿Remove-Item –path bin\Debug\package\* –recurse
+..\NuGet.exe pack . -OutputDirectory bin\Debug\package -IncludeReferencedProjects -Symbols -NonInteractive -Properties Configuration=Debug
+Dir .\bin\Debug\package\*.symbols.nupkg | rename-item -newname { [io.path]::ChangeExtension($_.name, "nupkg1") }
+Remove-Item –path bin\Debug\package\*.nupkg
+Dir .\bin\Debug\package\*.nupkg1 | rename-item -newname { [io.path]::ChangeExtension($_.name, "") }
+Dir .\bin\Debug\package\*.symbols | rename-item -newname { [io.path]::ChangeExtension($_.name, "nupkg") }
+..\Nuget.exe sources Add -Name "Micajah" -Source "https://micajah.pkgs.visualstudio.com/_packaging/Micajah/nuget/v3/index.json"
+..\Nuget.exe push -Source "Micajah" -ApiKey VSTS .\bin\Debug\package\*.nupkg
